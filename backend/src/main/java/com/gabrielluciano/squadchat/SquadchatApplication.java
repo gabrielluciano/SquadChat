6d14941b8ec0c8eae8eea85d689a365297b65df4
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-06-21 19:21:26 -0300

    create backend spring project

---
+++ backend/src/main/java/com/gabrielluciano/squadchat/SquadchatApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielluciano.squadchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquadchatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gabrielluciano/squadchat/SquadchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielluciano.squadchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
