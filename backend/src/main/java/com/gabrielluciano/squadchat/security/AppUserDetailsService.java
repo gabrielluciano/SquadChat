27780e498a38b7b492628177c569ba635b4240e7
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-07-18 23:41:35 -0300

    backend: add spring starter security dependency and create UserDetailsService

---
+++ backend/src/main/java/com/gabrielluciano/squadchat/security/AppUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.gabrielluciano.squadchat.security;
+
+import com.gabrielluciano.squadchat.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AppUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).map(SecurityUser::new)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
